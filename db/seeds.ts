83fee779d541a1f0fda2ef936e4661e515289650
Martin Moradi <moradi.martin@gmail.com>
2021-05-13 14:30:12 +0200

    add cleanDB before seed

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,4 +1,4 @@
-// import db from "./index"
+import db from "./index"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
@@ -8,9 +8,27 @@
  * realistic data.
  */
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  /*
+   * 1 - clean DB
+   */
+  cleanDB()
+  /*
+   * 2 - create Users
+   */
+}
+
+const cleanDB = async () => {
+  await db.user.deleteMany()
+  await db.session.deleteMany()
+  await db.token.deleteMany()
+  await db.ingredient.deleteMany()
+  await db.recipe.deleteMany()
+  await db.recipeIngredient.deleteMany()
+  await db.recipeTag.deleteMany()
+  await db.recipeUpvote.deleteMany()
+  await db.week.deleteMany()
+  await db.day.deleteMany()
+  console.log("db cleaned up")
 }
 
 export default seed
