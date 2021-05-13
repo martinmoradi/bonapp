99ac8bc4bd5921bec26ce344b9818caab48e9af9
Martin Moradi <moradi.martin@gmail.com>
2021-05-13 13:58:46 +0200

    migration addRecipeTagsOnRecipes

---
+++ db/migrations/20210513115737_add_recipe_tag/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "RecipeTag" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
